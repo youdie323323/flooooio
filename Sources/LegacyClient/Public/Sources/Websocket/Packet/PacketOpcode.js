dc57b4609d5e10e09fd40f541b6c888eb86f5112
youdi323323 <kouki32312171217@gmail.com>
2025-06-23 23:09:04 +0900

    Remove useless js files

--- Sources/LegacyClient/Public/Sources/Websocket/Packet/PacketOpcode.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DISPLAY_KICK_REASON = void 0;
-exports.DISPLAY_KICK_REASON = {
-    [0 /* ClientboundConnectionKickReason.OUTDATED_CLIENT */]: "Outdated client",
-    [1 /* ClientboundConnectionKickReason.CHEAT_DETECTED */]: "Cheat detected",
-};
